413f3057495ad3b7da1bc5e5d8217201a2dae298
NgTAnhMJB2306 <nta230620@gmail.com>
2023-02-09 21:19:48 +0700

    full code prj persimmon-hill-architects

---
+++ js/index.js
@@ -0,0 +1,27 @@
+// Fixed Header Navbar
+window.onscroll = function () { myFunction() };
+var header = document.getElementById("header");
+var sticky = header.offsetTop;
+
+function myFunction() {
+    if (window.pageYOffset > sticky) {
+        header.classList.add("sticky");
+    } else {
+        header.classList.remove("sticky");
+    }
+}
+
+// slider-img
+var myIndex = 0;
+carousel();
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("slide-img");
+    for (i = 0; i < x.length; i++) {
+      x[i].style.display = "none";  
+    }
+    myIndex++;
+    if (myIndex > x.length) {myIndex = 1}    
+    x[myIndex-1].style.display = "block";  
+    setTimeout(carousel, 3500);
+}
